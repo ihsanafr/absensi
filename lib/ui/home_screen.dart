f57baeeeee8d15e23a1f5d00ae5188cef74afdcf
ihsan <ihsan.ahmad2404@gmail.com>
2025-01-31 09:16:59 +0700

    new

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:facerecognation/ui/absen/absen_screen.dart';
 import 'package:facerecognation/ui/attend/attend_screen.dart';
 import 'package:facerecognation/ui/attendance_history/attendance_history_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -25,10 +26,25 @@ class HomeScreen extends StatelessWidget {
             // mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
-                child: Text('Halo, Selamat Datang!'),
+                
+                width: double.infinity,
+                color: Color(0xFF567DF5),
+                height: 150,
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Halo, Selamat Datang Di', style: GoogleFonts.poppins(color: Colors.white, fontSize: 20, fontWeight: FontWeight.normal,)),
+                    Text('Aplikasi Absensi', style: GoogleFonts.poppins(color: Colors.white, fontSize: 35, fontWeight: FontWeight.bold )),
+                  ],
+                ),
+                
               ),
               Container(
                 margin: const EdgeInsets.all(10),
+                width: 200,
+                color: Colors.amber,
                 child: InkWell(
                     highlightColor: Colors.transparent,
                     splashColor: Colors.transparent,
@@ -37,11 +53,12 @@ class HomeScreen extends StatelessWidget {
                     },
                     child: const Column(
                       children: [
-                        Image(image: AssetImage('assets/images/ic_absen.png'),
+                        Image(image: AssetImage('assets/images/absen.png'),
                           width: 100,
                           height: 100,
                         ),
                         SizedBox(height: 10,),
+
                         Text('Absen Kehadiran', style: TextStyle(
                           fontSize: 14,
                           fontWeight: FontWeight.bold
@@ -60,7 +77,7 @@ class HomeScreen extends StatelessWidget {
                     },
                     child: const Column(
                       children: [
-                        Image(image: AssetImage('assets/images/ic_leave.png'),
+                        Image(image: AssetImage('assets/images/izin.png'),
                           width: 100,
                           height: 100,
                         ),
@@ -83,7 +100,7 @@ class HomeScreen extends StatelessWidget {
                     },
                     child: const Column(
                       children: [
-                        Image(image: AssetImage('assets/images/ic_history.png'),
+                        Image(image: AssetImage('assets/images/riwayat.png'),
                           width: 100,
                           height: 100,
                         ),
